c7d1df921039abe9a3831ba5ba58e805feebc5cb
Nacho <nacho75xr@gmail.com>
2022-12-11 19:31:14 +0100

    Pre-deployment Vercel

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import { FC, PropsWithChildren } from 'react';
+import { Navbar } from '../Navbar';
+
+import styles from './MainLayout.module.css';
+
+export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home - Nacho</title>
+        <meta name='description' content='Home Page' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
